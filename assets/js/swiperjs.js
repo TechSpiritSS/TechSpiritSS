e86352f5cf189b92f0dae4295476f4ce7a75484b
Sidharth Sethi <Sidharth.sherry@gmail.com>
2021-12-22 17:43:53 +0530

    Portfolio

---
+++ assets/js/swiperjs.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".swiper", {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 10,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+  },
+});
